a6d5c7879c45ab994de42846ad9f1d79f009d276
danryland <danryland@users.noreply.github.com>
2023-10-08 22:22:49 +0000

    add edge function

---
+++ supabase/functions/telegram-bot/index.ts
@@ -0,0 +1,36 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts";
+
+console.log(`Function "telegram-bot" up and running!`);
+
+import { Bot, webhookCallback } from "https://esm.sh/grammy@1.19.1";
+
+const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
+
+bot.command("start", (ctx) => ctx.reply("Hi there! I'm Hammer"));
+
+bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
+
+const handleUpdate = webhookCallback(bot, "std/http");
+
+serve(async (req) => {
+  try {
+    const url = new URL(req.url);
+    if (url.searchParams.get("secret") !== Deno.env.get("FUNCTION_SECRET")) {
+      return new Response("not allowed", { status: 405 });
+    }
+
+    return await handleUpdate(req);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
